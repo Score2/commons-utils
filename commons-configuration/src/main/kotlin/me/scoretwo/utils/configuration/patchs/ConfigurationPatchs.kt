33a272106945f9eaa915cfc3cdb48b15a1f4249a
83669 <836696023@qq.com>
2020-10-24 16:44:11 +0800

    Added a configuration patch.

--- commons-configuration/src/main/kotlin/me/scoretwo/utils/configuration/patchs/ConfigurationPatchs.kt
+++ commons-configuration/src/main/kotlin/me/scoretwo/utils/configuration/patchs/ConfigurationPatchs.kt
@@ -2,7 +2,6 @@ package me.scoretwo.utils.configuration.patchs
 
 import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.configuration.MemorySection
-import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.configuration.file.YamlConfiguration
 import java.io.File
 
@@ -12,6 +11,25 @@ fun File.loadConfiguration(): YamlConfiguration {
 
 fun File.saveConfiguration(yamlConfiguration: YamlConfiguration) {
     yamlConfiguration.save(this)
+}
+
+fun ConfigurationSection.getLowerCaseNode(lowerCasePath: String): String {
+    val isMultiPath = lowerCasePath.contains(".")
+    val firstNode = if (!isMultiPath) lowerCasePath else lowerCasePath.substring(0, lowerCasePath.indexOf(".") - 1)
+
+    for (node in getKeys(false)) {
+        if (firstNode == node.toLowerCase()) {
+            return if (!isMultiPath) {
+                node
+            } else {
+                val afterNode = lowerCasePath.substring(lowerCasePath.indexOf("."))
+
+                "${node}.${getLowerCaseNode(afterNode)}"
+            }
+        }
+    }
+
+    return ""
 }
 
 fun ConfigurationSection.getConfigurationSectionList(path: String): List<ConfigurationSection>? {
