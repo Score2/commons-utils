3a1ec311c98ae83b1aa44370de229881a9fbb25f
Score2 <836696023@qq.com>
2021-02-08 22:42:11 +0800

    Update APIs.

--- commons-syntaxes/src/main/kotlin/me/scoretwo/utils/syntaxes/CollectionUtils.kt
+++ commons-syntaxes/src/main/kotlin/me/scoretwo/utils/syntaxes/CollectionUtils.kt
@@ -2,6 +2,4 @@ package me.scoretwo.utils.syntaxes
 
 import java.util.ArrayList
 
-fun <T> List<T>.toArrayList(): ArrayList<T> {
-    return ArrayList(this)
-}
+fun <T> List<T>.toArrayList() = ArrayList(this)
